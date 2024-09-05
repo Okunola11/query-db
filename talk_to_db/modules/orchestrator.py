383ac05aa84482314ccd7a9fa1fc1861e41a84f4
okunola <abdayoade@gmail.com>
2024-09-05 16:08:22 +0100

    refactor: implement spyware, agent instruments and validation
    spy_on_agents method was introduced to record the conversation between
    agents during the lifecyle. This helps out for future refrence and troubleshooting

--- talk_to_db/modules/orchestrator.py
+++ talk_to_db/modules/orchestrator.py
@@ -1,7 +1,11 @@
+import dataclasses
+import json
 from typing import List, Optional, Tuple
 import autogen
 
 from talk_to_db.modules import llm
+from talk_to_db.agents.instruments import AgentInstruments
+from talk_to_db.types import Chat, ConversationResult
 
 
 class Orchestrator:
@@ -14,16 +18,27 @@ class Orchestrator:
         name (str): Name of the Orchestrator.
         agents (List[autogen.ConversableAgent]): List of agents participating in the conversation.
         messages (List): History of messages exchanged during the conversation.
-        complete_keyword (str): Keyword signifying successful conversation completion (default: "APPROVED").
-        error_keyword (str): Keyword signifying conversation failure (default: "ERROR").
+        complete_keyword (str, optional): Keyword signifying successful conversation completion (default: "APPROVED").
+        error_keyword (str, optional): Keyword signifying conversation failure (default: "ERROR").
+        instruments (AgentInstruments): Reference to an object containing instrument functionalities (e.g., file access).
+        chats (List[Chat], optional): List of Chat objects representing individual message exchanges (default: []).
+        validate_results_func (callable, optional): Function used to validate conversation results.
     """
 
-    def __init__(self, name: str, agents: List[autogen.ConversableAgent]):
-        """Initializes the Orchestrator with a name and a list of agents.
+    def __init__(
+        self, 
+        name: str, 
+        agents: List[autogen.ConversableAgent],
+        instruments: AgentInstruments,
+        validate_results_func: callable = None
+    ):
+        """Initializes the Orchestrator with a name, list of agents, and instrument reference.
 
         Args:
             name (str): Name of the Orchestrator.
             agents (List[autogen.ConversableAgent]): List of conversational agents.
+            instruments (AgentInstruments): Reference to an object containing instrument functionalities (e.g., file access).
+            validate_results_func (callable, optional): Function used to validate conversation results (default: None).
 
         Raises:
             Exception: If the number of agents is less than 2.
@@ -34,6 +49,9 @@ class Orchestrator:
         self.messages = []
         self.complete_keyword = "APPROVED"
         self.error_keyword = "ERROR"
+        self.instruments = instruments
+        self.chats: List[Chat] = []
+        self.validate_results_func: callable = validate_results_func
 
         if len(self.agents) < 2:
             raise Exception("Orchestrator needs at least two agents")
@@ -63,6 +81,40 @@ class Orchestrator:
     @property
     def last_message_is_content(self):
         return self.last_message_is_dict and self.latest_message.get("content", None)
+
+    @property
+    def last_message_always_string(self):
+        if not self.messages:
+            return ""
+        if self.last_message_is_content:
+            return self.latest_message.get("content", "")
+        return str(self.messages[-1])
+
+    def send_message(
+        self,
+        from_agent: autogen.ConversableAgent,
+        to_agent: autogen.ConversableAgent,
+        message: str
+    ):
+        """Sends a message from one agent to another.
+
+        This method delegates the actual message sending to the `from_agent` object's `send` method. It also keeps track of the conversation history by adding a `Chat` object containing details about the message exchange.
+
+        Args:
+            from_agent (autogen.ConversableAgent): The agent sending the message.
+            to_agent (autogen.ConversableAgent): The agent receiving the message.
+            message (str): The message content to send.
+        """
+
+        from_agent.send(message, to_agent)
+
+        self.chats.append(
+            Chat(
+                from_name=from_agent.name,
+                to_name=to_agent.name,
+                message=str(message)
+            )
+        )
 
     def get_messages_as_string(self):
         """Concatenates all messages in the conversation history into a single string.
@@ -139,7 +191,7 @@ class Orchestrator:
 
         print(f"basic_chat(): '{agent_a.name}' --> '{agent_b.name}'")
 
-        agent_a.send(message, agent_b)
+        self.send_message(agent_a, agent_b, message)
 
         reply = agent_b.generate_reply(sender=agent_a)
 
@@ -166,11 +218,11 @@ class Orchestrator:
 
         print(f"memory_chat() '{agent_a.name}' --> '{agent_b.name}'")
 
-        agent_a.send(message, agent_b)
+        self.send_message(agent_a, agent_b, message)
 
         reply = agent_b.generate_reply(sender=agent_a)
 
-        agent_b.send(reply, agent_b)
+        self.send_message(agent_b, agent_b, reply)
 
         self.add_message(reply)
 
@@ -200,18 +252,61 @@ class Orchestrator:
 
         self.basic_chat(agent_a, agent_b, self.latest_message)
 
-    def sequential_conversation(self, prompt) -> Tuple[bool, List[str]]:
+    def self_function_chat(self, agent: autogen.ConversableAgent, message: str):
+        """Conducts a conversation where an agent performs a function call on itself.
+
+        This method sends the message to the agent and retrieves the generated reply. It adds the reply to the conversation history but does not send the reply back to the agent (unlike `function_chat`).
+
+        Args:
+            agent (autogen.ConversableAgent): The agent involved in the self-function call.
+            message (str): The message containing the function call.
+        """
+
+        print(f"self_function_chat(): {agent.name} --> {agent.name}")
+
+        self.send_message(agent, agent, message)
+
+        reply = agent.generate_reply(sender=agent)
+
+        # self.send_message(agent, agent, reply)
+
+        self.add_message(reply)
+
+        print(f"self_function_chat(): replied with:", reply)
+
+    def spy_on_agents(self, append_to_file: bool = True):
+        """Saves the conversation history to a file (optional).
+
+        This method iterates over the `chats` list and converts each `Chat` object to a dictionary using `dataclasses.asdict`. It then optionally writes the conversation history as a JSON-formatted string to a file specified by `self.instruments.agent_chat_file`.
+
+        Args:
+            append_to_file (bool, optional): Flag indicating whether to write the conversation to a file. Defaults to True.
+        """
+
+        conversations = []
+
+        for chat in self.chats:
+            conversations.append(dataclasses.asdict(chat))
+
+        if append_to_file:
+            file_name = self.instruments.agent_chat_file
+            with open(file_name, "w") as f:
+                f.write(json.dumps(conversations, indent=4))
+
+    def sequential_conversation(self, prompt) -> ConversationResult:
         """
         Runs a sequential conversation between agents, passing the prompt from one agent to the next in a chain.
 
-        For example
-            "Agent A" -> "Agent B" -> "Agent C" -> "Agent D" -> "Agent E"
+        For example:
+
+        "Agent A" -> "Agent B" -> "Agent C" -> "Agent D" -> "Agent E"
 
         Args:
             prompt (str): The initial prompt for the conversation.
 
         Returns:
-            Tuple[bool, List[str]]: A tuple containing whether the conversation was successful and the conversation history.
+            ConversationResult: A `ConversationResult` object containing details about the conversation,
+                            including success status, conversation history, cost, tokens, and the last message.
         """
 
         print(f"\n\n---------- {self.name} Orchestrator Starting----------\n\n")
@@ -234,34 +329,48 @@ class Orchestrator:
             if self.last_message_is_func_call and self.has_functions(agent_a):
                 self.function_chat(agent_a, agent_b, self.latest_message)
 
+            # custom spy on the conversation between agents
+            self.spy_on_agents()
+
             # Ending the process
             if idx == self.total_agents - 2:
+                if self.has_functions(agent_b):
+                    # agent_b -> func() -> agent_b
+                    self.self_function_chat(agent_b, self.latest_message)
+
                 print(f"---------- Orchestrator Complete ----------\n\n")
 
-                was_successful = self.complete_keyword in self.latest_message
+                was_successful = self.validate_results_func()
 
-                if was_successful:
-                    print("✅ Orchestrator was successful")
-                else:
-                    print("❌ Orchestrator failed")
+                self.spy_on_agents()
 
-                return was_successful, self.messages
+                cost, tokens = self.get_cost_and_tokens()
 
-    def broadcast_conversation(self, prompt: str) -> Tuple[bool, List[str]]:
+                return ConversationResult(
+                    success=was_successful,
+                    messages=self.messages,
+                    cost=cost,
+                    tokens=tokens,
+                    last_message_str=self.last_message_always_string
+                )
+
+    def broadcast_conversation(self, prompt: str) -> ConversationResult:
         """
         Broadcasts a prompt to all agents and collects their responses.
 
         For example:
-            "Agent A" -> "Agent B"
-            "Agent A" -> "Agent C"
-            "Agent A" -> "Agent D"
-            "Agent A" -> "Agent E"
+
+        - "Agent A" -> "Agent B"
+        - "Agent A" -> "Agent C"
+        - "Agent A" -> "Agent D"
+        - "Agent A" -> "Agent E"
 
         Args:
             prompt (str): The prompt to broadcast.
 
         Returns:
-            Tuple[bool, List[str]]: A tuple containing whether the broadcast was successful and the conversation history.
+            ConversationResult: A `ConversationResult` object containing details about the conversation,
+                            including success status, conversation history, cost, tokens, and the last message.
         """
 
         print(f"\n\n---------- {self.name} Orchestrator Starting ----------\n\n")
@@ -281,8 +390,23 @@ class Orchestrator:
             if self.last_message_is_func_call and self.has_functions(agent_iterate):
                 self.function_chat(agent_iterate, agent_iterate, self.latest_message)
 
+            self.spy_on_agents()
+
         print(f"---------- Orchestrator Complete ----------\n\n")
+        
+        was_successful = self.validate_results_func()
+        
+        if was_successful:
+            print(f"✅ Orchestrator was successful")
+        else:
+            print(f"❌ Orchestrator failed")
 
-        print(f"✅ Orchestrator was successful")
+        cost, token = self.get_cost_and_tokens()
 
-        return True, self.messages
+        return ConversationResult(
+            success=was_successful,
+            messages=self.messages,
+            cost=cost,
+            tokens=tokens,
+            last_message_str=self.last_message_always_string
+        )
